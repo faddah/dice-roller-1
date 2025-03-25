b4b40e40224c7b9c9f9ad9337f58115c7840a85a
Faddah Wolf <my_biz@me.com>
2025-03-25 12:30:59 -0700

    added dieSides addEventListener for "change" to disable all choices in numOfDice <select> if "100-10s" is chosen.

--- counter.js
+++ counter.js
@@ -5,6 +5,25 @@ const dieSides = document.querySelector("#die-sides-select");
 const numOfDice = document.querySelector("#number-of-dice-select");
 const rollButton = document.querySelector("#roll-button");
 const result = document.querySelector("#dice-results");
+dieSides.addEventListener("change", () => {
+    const numOfDiceOptions = numOfDice.querySelectorAll("option");
+    if (dieSides.value === "100-10s") {
+        numOfDiceOptions.forEach((option) => {
+            if (option.value === "1") {
+                option.disabled = false;
+                option.selected = true;
+            }
+            else {
+                option.disabled = true;
+            }
+        });
+    }
+    else {
+        numOfDiceOptions.forEach((option) => {
+            option.disabled = false;
+        });
+    }
+});
 rollButton.addEventListener("click", () => {
     if (!Number(dieSides.value) || !Number(numOfDice.value)) {
         alert("Please select a value for both fields.");
--- counter.ts
+++ counter.ts
@@ -13,6 +13,24 @@ const numOfDice = document.querySelector("#number-of-dice-select") as HTMLInputE
 const rollButton = document.querySelector("#roll-button") as HTMLButtonElement;
 const result = document.querySelector("#dice-results") as HTMLTextAreaElement;
 
+dieSides.addEventListener("change", () => {
+    const numOfDiceOptions = numOfDice.querySelectorAll("option");
+    if (dieSides.value === "100-10s") {
+        numOfDiceOptions.forEach((option: HTMLOptionElement) => {
+            if (option.value === "1") {
+                option.disabled = false;
+                option.selected = true;
+            } else {
+                option.disabled = true;
+            }
+        });
+    } else {
+        numOfDiceOptions.forEach((option: HTMLOptionElement) => {
+            option.disabled = false;
+        });
+    }
+});
+
 rollButton.addEventListener("click", () => {
     if (!Number(dieSides.value) || !Number(numOfDice.value)) {
         alert("Please select a value for both fields.");
