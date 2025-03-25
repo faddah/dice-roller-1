c73bb1d958593ef37f275142c71e3e96ade5898a
Faddah Wolf <my_biz@me.com>
2025-03-25 11:49:50 -0700

    Added conditional logic to handle the "100 sides in multiples of 10" choice.

--- diceRoller.js
+++ diceRoller.js
@@ -1,6 +1,16 @@
 const diceRoller = (dieSides, noOfDice = 1) => {
-    const indDieRoll = Array(noOfDice).fill(0).map(() => Math.floor(Math.random() * dieSides) + 1);
-    const total = indDieRoll.reduce((acc, current) => acc + current, 0);
+    let indDieRoll = [];
+    let total = 0;
+    if (dieSides === '100-10s') {
+        dieSides = 10;
+        indDieRoll = Array(noOfDice).fill(0).map(() => (Math.floor(Math.random() * Number(dieSides)) + 1) * 10);
+        total = indDieRoll.reduce((acc, current) => acc + current, 0);
+    }
+    else {
+        dieSides = Number(dieSides);
+        indDieRoll = Array(noOfDice).fill(0).map(() => Math.floor(Math.random() * Number(dieSides)) + 1);
+        total = indDieRoll.reduce((acc, current) => acc + current, 0);
+    }
     return { total, indDieRoll };
 };
 export default diceRoller;
